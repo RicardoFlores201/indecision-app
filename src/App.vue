c82c96ff13862c0278b58e1cf75bbe91fd61b1c0
richardram233 <richardram233@gmail.com>
2026-02-06 22:00:46 -0600

    Creación de componente MyCounter

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,9 @@
 <template>
-  <section>
-    <h3>Counter: {{ counter }}</h3>
-    <h3>Square: {{ square }}</h3>
-    <div>
-      <button v-on:click="add">+1</button>
-      <button v-on:click="substract">-1</button>
-    </div>
-  </section>
+  <h1>MI PRIMERA APP</h1>
+  <hr />
+  <MyCounter />
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
-
-//Variable reactiva
-const counter = ref(6);
-
-//Computed
-const square = computed(() => counter.value * counter.value);
-
-const add = () => counter.value++;
-
-const substract = () => counter.value--;
+import MyCounter from './components/MyCounter.vue';
 </script>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ square }}</h3>
+    <div>
+      <button v-on:click="add">+1</button>
+      <button v-on:click="substract">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+
+//Variable reactiva
+const counter = ref(6);
+
+//Computed
+const square = computed(() => counter.value * counter.value);
+
+const add = () => counter.value++;
+
+const substract = () => counter.value--;
+</script>
