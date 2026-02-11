b8f409ae4fbbde1195659af32a1356a97fe574a2
richardram233 <richardram233@gmail.com>
2026-02-11 14:07:49 -0600

    Visualización de respuestas y gift

--- src/composables/useIndecisionView.ts
+++ src/composables/useIndecisionView.ts
@@ -1,14 +1,33 @@
 import type { ChatMessage } from '@/interfaces/chat-message-interface';
+import type { YesNoResponse } from '@/interfaces/yes-no-response';
 import { ref } from 'vue';
 
 export function useIndecisionView() {
   const messages = ref<ChatMessage[]>([]);
 
-  function addUserMessage(text: string) {
+  const getResponse = async () => {
+    const resp = await fetch('https://yes-no-wtf.vercel.app/api');
+    const data = (await resp.json()) as YesNoResponse;
+    return data;
+  };
+
+  async function addUserMessage(text: string) {
+    if (text.length === 0) return;
+
     messages.value.push({
       id: Date.now(),
       itsMine: true,
       message: text,
+    });
+
+    if (!text.endsWith('?')) return;
+
+    const { answer, image } = await getResponse();
+    messages.value.push({
+      id: new Date().getTime(),
+      itsMine: false,
+      message: answer,
+      image: image,
     });
   }
 
