5c298bb294e208aea7ab82b13e54af177579c2b5
richardram233 <richardram233@gmail.com>
2026-02-11 13:43:26 -0600

    Eliminación de mensajes iniciales

--- src/composables/useIndecisionView.ts
+++ src/composables/useIndecisionView.ts
@@ -2,19 +2,7 @@ import type { ChatMessage } from '@/interfaces/chat-message-interface';
 import { ref } from 'vue';
 
 export function useIndecisionView() {
-  const messages = ref<ChatMessage[]>([
-    {
-      id: Date.now(),
-      message: 'PRIMER POSICIÓN ARREGLO',
-      itsMine: true,
-    },
-    {
-      id: Date.now() + 1,
-      message: 'SEGUNDA POSICIÓN ARREGLO',
-      itsMine: false,
-      image: 'https://yesno.wtf/assets/yes/4-c53643ecec77153eefb461e053fb4947.gif',
-    },
-  ]);
+  const messages = ref<ChatMessage[]>([]);
 
   function addUserMessage(text: string) {
     messages.value.push({
