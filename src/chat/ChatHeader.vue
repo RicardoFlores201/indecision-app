837636f3e1aba92569c399635437015c840cc64a
richardram233 <richardram233@gmail.com>
2026-02-11 12:53:22 -0600

    Separar componentes del chat layout

---
+++ src/chat/ChatHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+    <span>Mi esposa</span>
+  </div>
+</template>
--- src/views/IndecisionView.vue
+++ src/views/IndecisionView.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
-    <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
-      <span>Mi esposa</span>
-    </div>
+    <ChatHeader />
 
     <ChatMessages :messages="messages" />
 
@@ -14,6 +12,7 @@
 import ChatMessages from '@/chat/chatMessages.vue';
 import MessageBox from '@/chat/messageBox.vue';
 import { useIndecisionView } from '../composables/useIndecisionView';
+import ChatHeader from '@/chat/ChatHeader.vue';
 
 const { messages } = useIndecisionView();
 </script>
